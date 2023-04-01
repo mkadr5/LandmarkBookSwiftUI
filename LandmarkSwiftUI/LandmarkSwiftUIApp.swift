8e6a98bd58c7cdee2e7eac0dfaae37dd1cc84e87
mkadr5 <muhammetkadir52@gmail.com>
2023-04-01 17:28:26 +0300

    Initial Commit

---
+++ LandmarkSwiftUI/LandmarkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkSwiftUIApp.swift
+//  LandmarkSwiftUI
+//
+//  Created by Muhammet Kadir on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
